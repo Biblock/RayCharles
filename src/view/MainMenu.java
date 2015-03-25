f99b55f7d184ab2baef8c524fede9138da7e1bad
Pierre Massanès <mp405689@etu.unice.fr>
2015-03-25 09:01:53 +0100

    Creation of packages View and Engine

---
+++ src/view/MainMenu.java
@@ -0,0 +1,35 @@
+package view;
+
+import devintAPI.MenuAbstrait;
+
+public class MainMenu extends MenuAbstrait {
+
+	public MainMenu(String title) {
+		super(title);
+	}
+
+	@Override
+	protected String[] nomOptions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void lancerOption(int i) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected String wavAccueil() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String wavRegleJeu() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
