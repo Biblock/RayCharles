20a0f2f24961f65d735c832f02aea201a3aa07cd
Alexandre CAZALA <alexandre.cazala@gmail.com>
2015-03-25 09:16:48 +0100

    Interface IGame 1.0

--- src/engine/IGame.java
+++ src/engine/IGame.java
@@ -1,5 +1,9 @@
 package engine;
 
-public class IGame {
+import java.util.ArrayList;
 
+public interface IGame {
+	public boolean isCorrect(int Cursor);
+	public String randomSoundString(String pathFolder);
+	public void LoadSounds(String pathFolder, ArrayList<String> Sounds);	
 }
