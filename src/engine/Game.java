6e22988c156102f7122b31af396807a0d5fb46f4
Alexandre CAZALA <alexandre.cazala@gmail.com>
2015-03-25 10:57:50 +0100

    PUSH 2 DEATH !

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -13,29 +13,30 @@ public class Game implements IGame {
 	private int cursor;
 	private List<Sound> usedSounds;
 	private SIVOXDevint voix;
-	
-	public Game () {
+
+	public Game() {
 		soundSequence = new ArrayList<Integer>();
 		usedSounds = new ArrayList<Sound>();
 		usedSounds.add(Sound.BOING);
 		usedSounds.add(Sound.FUNNYSLIP);
 		usedSounds.add(Sound.METALCLANG);
-		
+
 		voix = new SIVOXDevint();
 		voix = Preferences.getData().getVoice();
 		System.out.println("HEYYYYYYY");
-		playAllSounds();
 		System.out.println("DONE");
 	}
+
 	@Override
 	public boolean isCorrect(int Cursor) {
 		// TODO Auto-generated method stub
 		return false;
 	}
-	
+
 	public void initialiazeCursor() {
 		cursor = 0;
 	}
+
 	public void incrementCursor() {
 		cursor++;
 	}
@@ -44,18 +45,20 @@ public class Game implements IGame {
 	public String randomSoundString(String pathFolder) {
 		return null;
 	}
-	
+
 	public void playAllSounds() {
 		for (Sound i : usedSounds) {
-			voix.playWav(i.getUrl(),true);
-	
+			voix.playWav(i.getUrl(), true);
+
 			System.out.println(i.getUrl());
 		}
 	}
+
 	public void generateSequence(int i) {
 		addRandomSound(i);
 		playAllSounds();
 	}
+
 	private void addRandomSound(int i) {
 		for (int j = 0; j < i; ++j) {
 			Random rand = new Random();
@@ -63,14 +66,14 @@ public class Game implements IGame {
 			soundSequence.add(new Integer(randomNum));
 		}
 	}
-	
+
 	private void addRandomSound() {
 		addRandomSound(1);
 	}
-	
+
 	@Override
 	public void LoadSounds(String pathFolder, ArrayList<String> Sounds) {
-		
+
 	}
-	
+
 }
