830f32b069632b90ff223a8b506a52aa25108ba8
Nicolas LECOURTOIS <ln404361@etu.unice.fr>
2015-03-31 22:24:10 +0200

    Je push aussi ce que j'ai fait

--- src/engine/Sound.java
+++ src/engine/Sound.java
@@ -3,7 +3,7 @@ package engine;
 public enum Sound {
     BOING("../ressources/sons/boing.wav"),
     FUNNYSLIP("../ressources/sons/funnySlip.wav"),
-    METALCLANG("../ressources/sons/metalClang.wav"),
+    METALCLANG("../ressources/sons/metalClang2.wav"),
     FAIL("../ressources/sons/fail.wav");
 
     public String getUrl() {
