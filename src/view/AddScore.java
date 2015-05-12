c01c4c7089f3391b56db3a8240a1bfd2771bdb75
Alexandre CAZALA <alexandre.cazala@gmail.com>
2015-05-12 16:54:49 +0200

    Version 1.0

--- src/view/AddScore.java
+++ src/view/AddScore.java
@@ -11,19 +11,26 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import jeu.Score;
+
 public class AddScore extends JFrame implements ActionListener{
 	private int score;
 	private JTextField name;
 	private JButton sendButton;
-	public AddScore (int score) {
+	private int numGame;
+	private int difficulte;
+	public AddScore (int numGame, int difficulte, int score) {
 		super("Demande de nom");
 		this.score = score;
+		this.numGame = numGame;
+		this.difficulte = difficulte;
 		JPanel Pane = new JPanel(new GridLayout(1, 2));
+		name = new JTextField();
 		Pane.add(name);
 		Pane.add(sendButton = nouveauBouton("Ajouter"));
 		this.setContentPane(Pane);
 		this.pack();
-
+		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
@@ -38,7 +45,7 @@ public class AddScore extends JFrame implements ActionListener{
 	public void actionPerformed(ActionEvent arg) {
 		if (arg.getSource().equals(sendButton)) {
 			String str = name.getText();
-			Score.addScore(name,score);
+			Score.addScore(numGame, difficulte, str,score);
 		}
 	}
 }
